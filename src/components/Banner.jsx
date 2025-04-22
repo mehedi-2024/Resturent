da520c3fdc5f180cc5cd47537ac261eb629fc805
mehedi-2024 <sheikhmh33@gmail.com>
2025-04-22 08:34:50 +0600

    edit banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/greencart_assets/assets'
 const Banner = () => {
     const asset = assets
   return (
-    <div className='relative w-full h-[100vh] lg:h-[70vh] lg'>
+    <div className='relative w-full h-[85vh] lg:h-[70vh]'>
         <div className='w-full h-full absolute top-0 left-0 '>
             <img src={asset.main_banner_bg} className='w-full h-full md:h-[65vh] hidden md:block' alt="" />
             <img src={asset.main_banner_bg_sm} className='w-full h-full md:hidden' alt="" />
