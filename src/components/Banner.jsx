a727fb3f237b30388c6fb3b1cf5a1d7f316b57fe
mehedi-2024 <sheikhmh33@gmail.com>
2025-04-22 10:17:51 +0600

    banner image change

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,14 +4,18 @@ import { assets } from '../assets/greencart_assets/assets'
 const Banner = () => {
     const asset = assets
     console.log(asset.main_banner_bg)
-  return (
-    <div className={`relative w-full h-[85vh] lg:h-[70vh] bg-[url('${asset.main_banner_bg}')] bg-cover bg-[80%] bg-no-repeat`}>
-        {/* <div className='w-full h-full absolute top-0 left-0 z-10'>
-            <img src={asset.main_banner_bg} className='w-full h-full md:h-[65vh] hidden md:block' alt="" />
-            <img src={asset.main_banner_bg_sm} className='w-full h-full md:hidden' alt="" />
-        </div> */}
-    </div>
-  )
+    return (
+        <div className={`relative w-full h-[85vh] lg:h-[85vh] bg-[url('https://images.unsplash.com/photo-1561758033-d89a9ad46330?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-[90%] bg-no-repeat text-center text-text2 bg-blend-overlay p-8 bg-black/70 flex items-center rounded-md`}>
+          
+            <div className='md:w-[60%] lg:w-[50%] md:text-left space-y-6'>
+                <h2 className='leading-snug tracking-tight'>Freshness You Can Trust, Savings You will Love!</h2>
+                <div className='flex gap-5 justify-center md:justify-start'>
+                    <button className='btn btn-primary'>Shop Now</button>
+                    <button className='hidden md:inline btn btn-info'>Explore More</button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Banner
